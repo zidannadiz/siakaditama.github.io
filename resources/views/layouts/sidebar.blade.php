786c8c41f310c46146c860921a9532e45cb71473
zidannadiz <zidanlangoet10987@gmail.com>
2026-09-06 07:38:02 +0700

    Fix KHS Fitures

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -276,6 +276,18 @@
                 </div>
             </a>
             @endif
+            
+            {{-- Master Template KRS/KHS: Admin PT --}}
+            @if(in_array($role, ['admin_pt', 'admin']))
+            <a href="{{ route('admin.template-krs-khs.index') }}" class="flex items-center justify-between px-4 py-3 rounded-lg transition-colors {{ str_starts_with($currentRoute, 'admin.template-krs-khs') ? 'bg-blue-50 text-blue-700' : 'text-gray-700 hover:bg-gray-50' }}">
+                <div class="flex items-center space-x-3">
+                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"></path>
+                    </svg>
+                    <span>Master Template</span>
+                </div>
+            </a>
+            @endif
             @endif
             @endif
 
